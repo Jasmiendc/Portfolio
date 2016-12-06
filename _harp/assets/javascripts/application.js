28c6342db9901b4ff1fd948ee98712ada1ed81dd
Jasmiendc <jasmien.declerck@student.ap.be>
2016-12-07 00:37:08 +0100

    eindversie commit

--- _harp/assets/javascripts/application.js
+++ _harp/assets/javascripts/application.js
@@ -1,6 +1,38 @@
 function main() {
-          // hier komt je code
-}
+  (function($) {
+              'use strict';
+            $('.isotope').each(function() {
+                  var $container = $(this);
+                  $container.imagesLoaded(function() {
+                      $container.isotope({
+                          itemSelector: '.isotope-item',
+                          percentPosition: true,
+
+                          masonry: {
+                              columnWidth: '.isotope-item'
+                          }
+                      });
+                  });
+              });
+
+
+              $('.filter li a').on('click', function() {
+                  $('.filter .active').removeClass('active');
+                  $(this).closest('li').addClass('active');
+                  var selector = $(this).attr('data-filter');
+                  $('.isotope').isotope({
+                      filter: selector,
+                      animationOptions: {
+                          duration: 500,
+                          queue: false
+                      }
+                  });
+                  return false;
+              });
+
+
+
+          })(jQuery);}
 
 window.onload = function() {
        main();
